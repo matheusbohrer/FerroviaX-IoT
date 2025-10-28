0a3f3886068bdc6f88336addf36b3908d86beb2c
matheusbohrer <matheusalvesbohrer@gmail.com>
2025-10-28 09:32:43 -0300

    env.h update

---
+++ trem/env.h
@@ -0,0 +1,21 @@
+#ifndef ENV_H
+#define ENV_H
+
+#define WIFI_SSID "FIESC_IOT_EDU"
+#define WIFI_PASS "8120gv08"
+
+#define BROKER_URL "bbbe195380654bf482d3272ea1f6c91f.s1.eu.hivemq.cloud"
+#define BROKER_PORT 8883
+
+// ----login criado para cada usuário no HiveMQ----
+#define BROKER_USR_ID "felipe1"
+#define BROKER_PASS_USR_PASS "Felipelucas1"
+
+#define BROKER_USR_ID "bohrer1"
+#define BROKER_PASS_USR_PASS "Matheus1"
+
+// ----a partir daqui depende da placa----
+#define TOPIC_PRESENCE1 ""
+...
+
+#endif
