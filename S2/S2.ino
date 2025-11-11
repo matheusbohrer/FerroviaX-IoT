1383e86f9d99628fbc0a4d4aefce90921dcb0046
cauachupel <caua_chupel@estudante.sesisenai.org.br>
2025-11-11 10:05:05 -0300

    update s2.ino

--- S2/S2.ino
+++ S2/S2.ino
@@ -1,43 +1,98 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include "env.h"  // arquivo com as configurações
 
-WiFiClient wifi_client;
-PubSubClient mqtt(wifi_client);
+// --- Pinos ---
+#define PIN_PRESENCA2 34
+#define PIN_PRESENCA4 35
+#define PIN_LED 2
 
-const String SSID = "FIESC_IOT_EDU";
-const String PASS = "8120gv08";
+WiFiClient wifiClient;
+PubSubClient mqtt(wifiClient);
 
-const String brokerURL = "test.mosquitto.org";
-const int brokerPort = 1883;
+unsigned long lastMsg = 0;
+int intervalo = 2000; // 2 segundos
 
-const String brokerUser = "";
-const String BrokerPass = "";
+// --- Callback MQTT ---
+void callback(char* topic, byte* payload, unsigned int length) {
+  String msg;
+  for (int i = 0; i < length; i++) msg += (char)payload[i];
+  Serial.print("Mensagem recebida em ");
+  Serial.print(topic);
+  Serial.print(": ");
+  Serial.println(msg);
+
+  // Controle LED
+  if (String(topic) == TOPIC_LED) {
+    if (msg == "1") {
+      digitalWrite(PIN_LED, HIGH);
+      Serial.println("LED ligado!");
+    } else {
+      digitalWrite(PIN_LED, LOW);
+      Serial.println("LED desligado!");
+    }
+  }
+}
+
+// --- Reconexão MQTT ---
+void reconnect() {
+  while (!mqtt.connected()) {
+    Serial.print("Tentando conectar ao broker MQTT...");
+    if (mqtt.connect(DEVICE_NAME, BROKER_USER_ID, BROKER_USER_PASS)) {
+      Serial.println("Conectado!");
+      mqtt.subscribe(TOPIC_LED);
+      mqtt.subscribe(TOPIC_SERVO3);
+    } else {
+      Serial.print("Falha, rc=");
+      Serial.print(mqtt.state());
+      Serial.println(" tentando novamente em 5s");
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
-  Serial.begin(115200);    //configura a placa pra mostrar na tela
-  WiFi.begin(SSID, PASS);  //tenta conectar na rede
-  Serial.println("Conectando no WiFi");
+  Serial.begin(115200);
+  pinMode(PIN_PRESENCA2, INPUT);
+  pinMode(PIN_PRESENCA4, INPUT);
+  pinMode(PIN_LED, OUTPUT);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.print("Conectando-se ao Wi-Fi");
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.println(".");
-    delay(2000);
-  }
-  Serial.println("Conectado com sucesso");
-  mqtt.setServer(brokerURL.c_str(),brokerPort);
-  String clientID = "estação_principal";
-  clientID += String(random(0xffff), HEX);
-  while(mqtt.connect(clientID.c_str()) == 0 ){
+    delay(500);
     Serial.print(".");
-    delay(200);
   }
-  Serial.println("\n Conectado ao broker!");
+  Serial.println("\nWi-Fi conectado!");
+  Serial.println(WiFi.localIP());
+
+  mqtt.setServer(BROKER_URL, BROKER_PORT);
+  mqtt.setCallback(callback);
 }
 
 void loop() {
- String mensagem = "";
- if(Serial.available() > 0){
-  mensagem = Serial.readStringUntil('\n');
-  Serial.println("A mensagem foi: ", mensagem);
-  Serial.println(mensagem);
- }
+  if (!mqtt.connected()) reconnect();
+  mqtt.loop();
 
+  unsigned long agora = millis();
+  if (agora - lastMsg > intervalo) {
+    lastMsg = agora;
+
+    int presenca2 = digitalRead(PIN_PRESENCA2);
+    int presenca4 = digitalRead(PIN_PRESENCA4);
+
+    char msgPresenca2[8];
+    char msgPresenca4[8];
+    sprintf(msgPresenca2, "%d", presenca2);
+    sprintf(msgPresenca4, "%d", presenca4);
+
+    mqtt.publish(TOPIC_PRESENCA2, msgPresenca2);
+    mqtt.publish(TOPIC_PRESENCA4, msgPresenca4);
+
+    Serial.println("Dados enviados:");
+    Serial.print("Presença 2: ");
+    Serial.println(presenca2);
+    Serial.print("Presença 4: ");
+    Serial.println(presenca4);
+  }
 }
