bb7b892ff9c0bf1ffffb36126e6411bb73d64d49
Matheus Alves Bohrer <160776008+matheusbohrer@users.noreply.github.com>
2025-12-01 21:23:15 -0300

    Update HiveMQ broker configuration details

--- S3/env.h
+++ S3/env.h
@@ -6,10 +6,10 @@
 #define WIFI_PASS "8120gv08"
 
 // --- HiveMQ ---
-#define BROKER_URL "179.222.224.26"
-#define BROKER_PORT 6883
-#define BROKER_USER_ID "nextrain"
-#define BROKER_USER_PASS "nextrain"
+#define BROKER_URL "bbbe195380654bf482d3272ea1f6c91f.s1.eu.hivemq.cloud"
+#define BROKER_PORT 8883
+#define BROKER_USER_ID "felipe1"
+#define BROKER_USER_PASS "Felipelucas1"
 
 // --- Identificação do Dispositivo
 #define DEVICE_NAME "S3"
